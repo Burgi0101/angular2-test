164b7c4e12277924f854989b1271651d176457b4
Burgi0101 <georg.burgstaller@gmx.at>
2016-11-21 14:21:53 +0100

    added style changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Portfolio Page!</h1>'
+    template: `<h1>This is {{ name }}'s Portfolio Page!</h1>`
 })
-export class AppComponent { }
+export class AppComponent {
+
+    name: string;
+
+    constructor(){
+        this.name = 'Georg';
+    }
+ }
