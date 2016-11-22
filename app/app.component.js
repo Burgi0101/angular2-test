453eb405e492f2cf697a8f7a12883c8a287a405e
Burgi0101 <georg.burgstaller@gmx.at>
2016-11-22 15:29:24 +0100

    renamed my-app to app

--- app/app.component.js
+++ app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     }
     AppComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
+            selector: 'app',
             template: "<h1>This is {{ name }}'s Portfolio Page!</h1>"
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `<h1>This is {{ name }}'s Portfolio Page!</h1>`
 })
 export class AppComponent {
