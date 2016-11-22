5d05eca91af98f4ef592df17dcaa1850d12f6cae
Burgi0101 <georg.burgstaller@gmx.at>
2016-11-22 18:28:55 +0100

    added loading animation to the page

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoadingComponent } from './components/loading-component/loading.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, LoadingComponent ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
